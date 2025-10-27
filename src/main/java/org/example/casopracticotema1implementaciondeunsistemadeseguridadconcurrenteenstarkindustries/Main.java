4a9807c396c9e165d47c4cec9fb6e54a42eca8aa
gabriel140282 <gkaakmor@myuax.com>
2025-10-27 22:41:56 +0100

    V0.1.1

---
+++ src/main/java/org/example/casopracticotema1implementaciondeunsistemadeseguridadconcurrenteenstarkindustries/Main.java
@@ -0,0 +1,13 @@
+package org.example.casopracticotema1implementaciondeunsistemadeseguridadconcurrenteenstarkindustries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasoPracticoTema1ImplementacionDeUnSistemaDeSeguridadConcurrenteEnStarkIndustriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasoPracticoTema1ImplementacionDeUnSistemaDeSeguridadConcurrenteEnStarkIndustriesApplication.class, args);
+	}
+
+}
